4defa4a9b40ed8dbb59024a728fc5ba518f62f11
Bora IŞIKOĞLU <79979746+boraisikoglu@users.noreply.github.com>
2021-05-29 20:30:51 +0300

    Add files via upload

---
+++ Client/app/src/main/java/com/example/moviesstreamingappclient/Model/MovieItemClickListenerNew.java
@@ -0,0 +1,8 @@
+package com.example.moviesstreamingappclient.Model;
+
+import android.widget.ImageView;
+
+public interface MovieItemClickListenerNew {
+    void onMovieClick(GetVideoDetails movie, ImageView movieImageView);
+}
+
